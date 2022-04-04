df15e9cb70c42ef7ef4fb41c75fde43971e6e01d
rs0122 <rs.0122.ssthe2@hotmail.co.jp>
2022-04-04 11:14:29 +0900

    データクラス

--- src/main/kotlin/chapter2/Main.kt
+++ src/main/kotlin/chapter2/Main.kt
@@ -38,6 +38,42 @@ fun main(args: Array<String>) {
     //hashCode関数、equals関数での比較
     val set = hashSetOf(userA)
     println(set.contains(userB))
+
+    //データクラス
+    //リスト2.3.6
+    //アクセサメソッド
+    val user7 = User7(1, "Sauti")
+    user.name = "kotukn"
+    println(user.name)
+
+    //リスト2.3.7
+    //equals
+    val user71 = User7(1, "Sanio")
+    val same = User7(1, "Sanio")
+    val other = User7(2, "Sanio")
+    println(user71 == same)
+    println(user71 == other)
+
+    //hashcode
+    //リスト2.3.8
+    val user72 = User7(1,"Takegana")
+    val same72 = User7(1,"Takegana")
+    val other72 = User7(2,"Takegana")
+    println("user=${user72.hashCode()}")
+    println("same=${same72.hashCode()}")
+    println("same=${other72.hashCode()}")
+    val set72 = hashSetOf(user72)
+    println(set72.contains(same72))
+    println(set72.contains(other72))
+    //リスト2.3.9
+    println(user72.toString())
+    //componentN
+    println(user72.component1())
+    println(user72.component2())
+    //copy関数
+    val updated = user72.copy(1, "Kotlin")
+    println(updated.toString())
+
 }
 
 //リスト2.1.1
@@ -106,7 +142,42 @@ class User5 {
 }
 
 //リスト2.3.1
+//class User6{
+//    val id: Int = 1
+//    val name = "Kotlin"
+//}
+
+//リスト2.3.3
 class User6{
     val id: Int = 1
     val name = "Kotlin"
+
+    override fun equals(other: Any?):Boolean{
+        if(this === other) return true
+        if(javaClass != other?.javaClass) return false
+
+        other as User6
+
+        if(id != other.id) return false
+        if(name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return 31 *name.hashCode() + id
+    }
+
+    override fun toString(): String {
+        return "User6(id=$id, name=$name)W"
+    }
 }
+
+//データクラスの定義
+//リスト2.3.5
+//リスト2.3.12(拡張プロパティ)
+data class User7(val id: Int, val name: String){
+    val isValidName: Boolean
+    get() = name != ""
+}
+
